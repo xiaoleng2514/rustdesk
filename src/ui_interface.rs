13502d9f0413e4a452d877b2d927956b1f111fd3
xiaoleng2514 <168867025+xiaoleng2514@users.noreply.github.com>
2024-12-17 21:37:56 +0800

    Update ui_interface.rs

--- src/ui_interface.rs
+++ src/ui_interface.rs
@@ -874,7 +874,7 @@ pub fn video_save_directory(root: bool) -> String {
     #[cfg(any(target_os = "android", target_os = "ios"))]
     if let Ok(home) = config::APP_HOME_DIR.read() {
         let mut path = home.to_owned();
-        path.push_str("/yuancheng/ScreenRecord");
+        path.push_str("/RustDesk/ScreenRecord");
         let dir = try_create(&std::path::Path::new(&path));
         if !dir.is_empty() {
             return dir;
